286533610c11c19e086cc9adf721484f16fbe059
Hg80 <155629455+Hg80Mer@users.noreply.github.com>
2026-01-20 00:06:08 +0900

    알람 등록 화면

---
+++ app/src/main/java/com/example/miraclemorning/utils/MyAlarmSetupActivity.kt
@@ -0,0 +1,19 @@
+package com.example.miraclemorning.utils
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.miraclemorning.R
+
+class MyAlarmSetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_alarm_setup)
+
+        findViewById<Button>(R.id.btnSetAlarms).setOnClickListener {
+            MyAlarmUtil.setQuoteAlarm(this, 6, 0, 2001)
+            MyAlarmUtil.setQuoteAlarm(this, 12, 0, 2002)
+            MyAlarmUtil.setQuoteAlarm(this, 20, 0, 2003)
+        }
+    }
+}
